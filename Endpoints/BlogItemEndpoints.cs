8cfa010f80ffb71a6a335a4ca0906b005ea03eb7
dkj121 <2726073422@qq.com>
2026-07-24 18:56:01 +0800

    Refactor API endpoint documentation for BlogItem endpoints

--- Endpoints/BlogItemEndpoints.cs
+++ Endpoints/BlogItemEndpoints.cs
@@ -9,35 +9,24 @@ namespace SASTCsharpBlogPart.Endpoints
 {
 	/// <summary>
 	/// 将 BlogItem 相关的 API 端点映射到路由构建器的扩展方法类。
+	/// GET: /api/blogitem - 获取所有博客项
+	/// GET: /api/blogitem/{id} - 根据 ID 获取单个博客项
+	/// PUT: /api/blogitem/{id} - 更新指定 ID 的博客项
+	/// POST: /api/blogitem - 创建新的博客项
+	/// DELETE: /api/blogitem/{id} - 删除指定 ID 的博客项
 	/// </summary>
 	public static class BlogItemEndpoints
 	{
-		/// <summary>
-		/// 将 BlogItem 相关的 API 端点映射到路由构建器。
-		/// </summary>
-		/// <param name="routes">路由构建器</param>
 		public static void MapBlogItemEndpoints(this IEndpointRouteBuilder routes)
 		{
 			var group = routes.MapGroup("/api/blogitem").WithTags(nameof(BlogItem));
 
-			/// <summary>
-			/// 返回所有博客项的列表。
-			/// </summary>
-			/// <param name="db">数据库上下文</param>
-			/// <returns>所有博客项的列表</returns>
 			group.MapGet("/", async (BlogItemContext db) =>
 			{
 				return await db.BlogItem.ToListAsync();
 			})
 			.WithName("GetAllBlogItems");
 
-			/// <summary>
-			/// 根据 ID 返回特定的博客项。
-			/// </summary>
-			/// <param name="Task<Results<Ok<BlogItem>"></param>
-			/// <param name="id">博客项的 ID</param>
-			/// <param name="db">数据库上下文</param>
-			/// <returns>返回特定博客项的结果</returns>
 			group.MapGet("/{id}", async Task<Results<Ok<BlogItem>, NotFound>> (int id, BlogItemContext db) =>
 			{
 				return await db.BlogItem.AsNoTracking()
@@ -48,13 +37,6 @@ namespace SASTCsharpBlogPart.Endpoints
 			})
 			.WithName("GetBlogItemById");
 
-			/// <summary>
-			/// 根据 ID 更新特定的博客项。
-			/// </summary>
-			/// <param name="id">博客项的 ID</param>
-			/// <param name="blogitem">更新后的博客项数据</param>
-			/// <param name="db">数据库上下文</param>
-			/// <returns>更新的结果</returns>
 			group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, BlogItem blogitem, BlogItemContext db) =>
 			{
 				var affected = await db.BlogItem
@@ -73,12 +55,6 @@ namespace SASTCsharpBlogPart.Endpoints
 			})
 			.WithName("UpdateBlogItem");
 
-			/// <summary>
-			/// 创建新的博客项。
-			/// </summary>
-			/// <param name="blogitem">新的博客项数据</param>
-			/// <param name="db">数据库上下文</param>
-			/// <returns>创建博客项的结果</returns>
 			group.MapPost("/", async (BlogItem blogitem, BlogItemContext db) =>
 			{
 				db.BlogItem.Add(blogitem);
@@ -87,12 +63,6 @@ namespace SASTCsharpBlogPart.Endpoints
 			})
 			.WithName("CreateBlogItem");
 
-			/// <summary>
-			/// 根据 ID 删除特定的博客项。
-			/// </summary>
-			/// <param name="id">博客项的 ID</param>
-			/// <param name="db">数据库上下文</param>
-			/// <returns>删除博客项的结果</returns>
 			group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, BlogItemContext db) =>
 			{
 				var affected = await db.BlogItem
